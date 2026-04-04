88c8a8052910203f80d33e415414d7820b349158
Davide Palma (ElDavo) <github@davidepalma.it>
2026-04-04 20:32:12 +0200

    bump ezxposed

--- xposed/src/main/java/icu/nullptr/playintegritybreak/xposed/XposedEntry.kt
+++ xposed/src/main/java/icu/nullptr/playintegritybreak/xposed/XposedEntry.kt
@@ -1,6 +1,6 @@
 package icu.nullptr.playintegritybreak.xposed
 
-import com.github.kyuubiran.ezxhelper.init.EzXHelperInit
+import io.github.kyuubiran.ezxhelper.xposed.EzXposed
 import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.IXposedHookZygoteInit
 import de.robv.android.xposed.callbacks.XC_LoadPackage
@@ -14,7 +14,7 @@ class XposedEntry : IXposedHookZygoteInit, IXposedHookLoadPackage {
     private val integrityHooksInstalled = AtomicBoolean(false)
 
     override fun initZygote(startupParam: IXposedHookZygoteInit.StartupParam) {
-        EzXHelperInit.initZygote(startupParam)
+        EzXposed.initZygote(startupParam)
     }
 
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
@@ -22,7 +22,7 @@ class XposedEntry : IXposedHookZygoteInit, IXposedHookLoadPackage {
         if (lpparam.processName != Constants.VENDING_PACKAGE_NAME) return
         if (!lpparam.isFirstApplication) return
 
-        EzXHelperInit.initHandleLoadPackage(lpparam)
+        EzXposed.initHandleLoadPackage(lpparam)
         if (!integrityHooksInstalled.compareAndSet(false, true)) return
 
         runCatching {
